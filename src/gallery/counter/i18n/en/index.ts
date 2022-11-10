4a4413892a35d188b1c8c326146a307c6dcc41cd
renyifei <renyifei@agora.io>
2022-11-10 19:45:49 +0800

    first commit

---
+++ src/gallery/counter/i18n/en/index.ts
@@ -0,0 +1,10 @@
+const countdown_en = {
+  widget_countdown: {
+    appName: 'Countdown',
+    start: 'Start',
+    seconds: 's',
+    restart: 'Restart',
+  },
+};
+
+export default countdown_en;
---
+++ src/gallery/counter/i18n/zh/index.ts
@@ -0,0 +1,10 @@
+const countdown_zh = {
+  widget_countdown: {
+    appName: '计时器',
+    start: '开始',
+    seconds: '秒',
+    restart: '重新开始',
+  },
+};
+
+export default countdown_zh;
