5a7e0e6972b8b1487410909d956262a897df9c26
zhanheng <zhanheng@agora.io>
2023-06-14 20:48:58 +0800

    fix: postcss plugin

--- postcss.config.js
+++ postcss.config.js
@@ -4,5 +4,20 @@ const tailwindConfig = require('./tailwind.config');
 const plugin = require('agora-common-libs/presets/postcss-plugin/px-to-vw/index.js');
 
 module.exports = {
-  plugins: [autoprefixer(), tailwindcss(tailwindConfig), plugin],
+  plugins: [
+    autoprefixer(),
+    tailwindcss(tailwindConfig),
+    plugin({
+      viewportWidth: 375,
+      unitPrecision: 5,
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      include: [/\/mobile\//, /\.mobile\./],
+      exclude: [/\/node_modules\//i],
+      landscape: true, // 是否处理横屏情况
+      landscapeUnit: 'vw', // (String) 横屏时使用的单位
+      landscapeWidth: 812, // (Number) 横屏时使用的视口宽度
+      landscapeHeight: 375, // (Number) 横屏时使用的视口宽度
+    }),
+  ],
 };
