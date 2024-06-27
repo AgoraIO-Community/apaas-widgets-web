dbcc7857f5ada7fb1de6374aecf182717c31f4a2
wangliang <wangliang_workjob@163.com>
2024-06-27 20:14:24 +0800

    修复dev编译问题

--- src/gallery/classroom/hx-chat/fcr-chatroom/store/user.ts
+++ src/gallery/classroom/hx-chat/fcr-chatroom/store/user.ts
@@ -27,7 +27,6 @@ export class UserStore {
   private _raiseHandTooltipTask: Scheduler.Task | null = null;
   
   constructor(private _widget: AgoraHXChatWidget, private _fcrChatRoom: AgoraIMBase) {
-    this._privateUser = this._fcrChatRoom.getPrivateUser()
     this._addEventListeners();
     this._onUserJoined = this._onUserJoined.bind(this);
     this._initUserMuted();
@@ -118,16 +117,13 @@ export class UserStore {
     });
     
   }
-  @observable 
-  private _privateUser?: AgoraIMUserInfo;
   @computed
   get privateUser() {
-    return this._privateUser;
+    return this._fcrChatRoom.getPrivateUser();
   }
   @action.bound
   setPrivateUser(user: AgoraIMUserInfo | undefined) {
     this._fcrChatRoom.setPrivateUser(user)
-    this._privateUser = user;
   }
 
   @action.bound
